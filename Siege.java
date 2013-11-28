d8556115b21196758eeacdc5c101e8f51baaa065
Maximilian Hemmrich <maximilian.hemmrich@gmail.com>
2013-11-28 15:17:47 -0500

    Fixed Server Communication
    Made the string returned by the server to the client readable

--- Siege.java
+++ Siege.java
@@ -1,6 +1,5 @@
 package com.eecs285.siegegame;
 
-import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -12,73 +11,139 @@ import com.eecs285.siegegame.ActionParser.ActionType;
 
 public class Siege {
 
-// These are required for the classes to work properly:
-	public static Grid grid;
-	public static int numCities;
-	public static int currentPlayer;
-	public static Player []players;
-// -- Sinan
+    // These are required for the classes to work properly:
+    public static Grid grid;
+    public static int numCities;
+    public static int currentPlayer;
+    public static Player[] players;
 
+    // required for networking functionality
     static BufferedReader in;
     static DataOutputStream out;
     static int portNum = 45000;
-    
-    //change this to whatever IP address the server has
-    private final static String IPaddress = "127.0.0.1"; // localhost
-                                                         
+    private final static String IPaddress = "127.0.0.1"; // server IP
 
     public static void main(String args[]) throws Exception {
-//    initialize connection to server (including IO streams)
-//    initServerConnection();
+        // initialize connection to server (including IO streams)
+        initServerConnection();
 
-      final int rows = 25;
-      final int cols = 40;
-      
-      Tile[][] mapTiles = new Tile[rows][cols];
-      for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-          mapTiles[i][j] = new TilePlains(new Coord(i, j));
-      
-      MainGameFrame mainFrame = new MainGameFrame("Siege", rows, cols, mapTiles);
-      mainFrame.printNarration("Jordan added a bit of functionality");
-      mainFrame.printNarration("Jordan tested the functionality");
-      mainFrame.printNarration("Jordan is making sure the scrollbar works");
-      for (int i = 0; i < 50; i++)
-        mainFrame.printNarration("Filling in 50 lines...");
-      mainFrame.printNarration("Jordan ended turn");
+        final int rows = 25;
+        final int cols = 40;
 
-      
-      Coord oneOne = new Coord(1, 1);
-      Tile plains = new TilePlains(oneOne);
-      mainFrame.updateGridSquare(oneOne, plains);
-      
-//    while (true) {
-//    String fromServer = in.readLine();
-//    System.out.println("FROM SERVER: " + fromServer);
-//    }
-      
-      //max testing ActionParser
-      /*
-      String test = "something sinister attacks something good";
-      ActionType atype = ActionParser.getActionType(test);
-      System.out.println(test);
-      if(atype == ActionType.ATTACK) System.out.println("Attack");
-      
-      test = "Player 1 captures city from Player 2";
-      atype = ActionParser.getActionType(test);
-      System.out.println(test);
-      if(atype == ActionType.CAPTURE_CITY) System.out.println("Capture city");
-      
-      test = "asdfajsdflkja ends turn";
-      atype = ActionParser.getActionType(test);
-      System.out.println(test);
-      if(atype == ActionType.END_TURN) System.out.println("End turn");
-      
-      Coord coord = ActionParser.getFirstCoordinate("Army at (6, 1) attacked army at (19,51)");
-      System.out.println("row = " + coord.row + "   col = " + coord.col);
-      coord = ActionParser.getSecondCoordinate("Army at (6, 1) attacked army at (19,51)");
-      System.out.println("row = " + coord.row + "   col = " + coord.col); 
-      */
+        Tile[][] mapTiles = new Tile[rows][cols];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                mapTiles[i][j] = new TilePlains(new Coord(i, j));
+
+        MainGameFrame mainFrame = new MainGameFrame("Siege", rows, cols,
+                mapTiles);
+        mainFrame.printNarration("Jordan added a bit of functionality");
+        mainFrame.printNarration("Jordan tested the functionality");
+        mainFrame.printNarration("Jordan is making sure the scrollbar works");
+        for (int i = 0; i < 50; i++)
+            mainFrame.printNarration("Filling in 50 lines...");
+        mainFrame.printNarration("Jordan ended turn");
+
+        Coord oneOne = new Coord(1, 1);
+        Tile plains = new TilePlains(oneOne);
+        mainFrame.updateGridSquare(oneOne, plains);
+        
+        //testing max
+        //sendToServer("someone attacks someone else");
+        sendToServer("Player's army at (1, 2) attacks player's army/city at (3, 4)");
+
+        while (true) {
+            String fromServer = in.readLine();
+            System.out.println("FROM SERVER: " + fromServer);
+            fromServer = trimServerString(fromServer); 
+            
+            ActionType atype = ActionParser.getActionType(fromServer);
+            Coord attacker = null, target = null;
+            Coord city = null, resource = null;
+            
+            if(atype == null) {
+                System.out.println("ERROR: atype is null");
+                System.exit(-1);
+            }
+            
+            switch(atype) {
+            case ATTACK:
+                System.out.println("Attack");
+                attacker = ActionParser.getFirstCoordinate(fromServer);
+                target = ActionParser.getSecondCoordinate(fromServer);
+                System.out.println("attacker at (" + attacker.row + ", " + attacker.col + ")");
+                System.out.println("target at (" + target.row + ", " + target.row + ")");
+                
+                break;
+            case CAPTURE_CITY:
+                System.out.println("Capture City");
+                city = ActionParser.getFirstCoordinate(fromServer);
+                
+                break;
+            case CAPTURE_RESOURCE:
+                System.out.println("Capture Resource");
+                resource = ActionParser.getFirstCoordinate(fromServer);
+                
+                break;
+            case CITY_LIBERATED:
+                System.out.println("Liberate City");
+                city = ActionParser.getFirstCoordinate(fromServer);
+                
+                break;
+            case CITY_UNDER_SIEGE:
+                System.out.println("Siege City");
+                city = ActionParser.getFirstCoordinate(fromServer);
+                
+                break;
+            case END_TURN:
+                System.out.println("End Turn");
+                break;
+            case LOSE_UNITS:
+                System.out.println("Lose Units");
+                break;
+            case MERGE_ARMY:
+                System.out.println("Merge Army");               
+                break;
+            case MOVE_ARMY:
+                System.out.println("Move Army");
+                break;
+            case RECRUIT:
+                System.out.println("Recruit");
+                break;
+            case RESOURCE_RECAPTURED:
+                System.out.println("Recapture Resource");
+                resource = ActionParser.getFirstCoordinate(fromServer);
+                
+                break;
+            case RESOURCE_UNDER_CONFLICT:
+                System.out.println("Resource Contested");
+                resource = ActionParser.getFirstCoordinate(fromServer);
+                
+                break; 
+            case PLAYER_DEFEATED:
+                System.out.println("Player Defeated");
+                break;
+            case PLAYER_WINS:
+                System.out.println("Player Wins");
+                break;
+            default:
+                System.out.println("ERROR: Action did not specify a known ActionType");
+                System.exit(-1);
+                break;           
+            }            
+        }
+    }
+
+    private static String trimServerString(String fromServer) {
+        // string from server has 3 space chars between each usable char
+        // need to trim the spaces to use string.contains() method
+        
+        String usableData = "";
+        for(int i = 0; i < fromServer.length(); i++)
+            if((i + 1)%4 == 0) //every fourth character is useful
+                usableData += String.valueOf(fromServer.charAt(i));
+        
+        return usableData;
     }
 
     private static void initServerConnection() throws Exception {
