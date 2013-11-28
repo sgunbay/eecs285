26ea3b1dcf6e605116b2e4b89847eb207e3a4683
Jordan Liu <jminjie@umich.edu>
2013-11-27 23:58:52 -0500

    GUI Update with Name Prompt
    Also demonstration of outlineAroundSquare

---
+++ GetNameDialog.java
@@ -0,0 +1,69 @@
+package com.eecs285.siegegame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class GetNameDialog extends JDialog {
+
+  JTextField inputField;
+  JButton okButton;
+  private static final long serialVersionUID = 1L;
+  
+  public GetNameDialog(JFrame mainFrame){
+    super(mainFrame, "", true);
+    
+    ImageIcon title = new ImageIcon("src/Resources/titlecard.png");
+    
+    JPanel topUpperPan = new JPanel(new FlowLayout());
+    JPanel topLowerPan = new JPanel(new FlowLayout());
+    JPanel topPan = new JPanel(new BorderLayout());
+    JPanel bottomPan = new JPanel(new FlowLayout());
+    setLayout(new BorderLayout());
+    okButton = new JButton("OK");
+    bottomPan.add(okButton);
+    topUpperPan.add(new JLabel("Player name:", SwingConstants.LEFT));
+    inputField = new JTextField(40);
+    topLowerPan.add(inputField);
+    
+    topPan.add(new JLabel(title), BorderLayout.NORTH);
+    topPan.add(topUpperPan, BorderLayout.CENTER);
+    topPan.add(topLowerPan, BorderLayout.SOUTH);
+    add(topPan, BorderLayout.CENTER);
+    add(bottomPan, BorderLayout.SOUTH);
+    
+    buttonListener listener = new buttonListener();
+    okButton.addActionListener(listener);
+
+    pack();
+    setResizable(false);
+    setLocation(400,200);
+    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    getRootPane().setDefaultButton(okButton);
+    setVisible(true);
+  }
+  
+  public class buttonListener implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+      if (e.getSource() == okButton){
+        if (getInput().compareTo("") != 0)
+          //button won't close if nothing has been entered
+          setVisible(false);
+      }
+    }
+  }
+  
+  public String getInput(){
+    return inputField.getText();
+  }
+}
--- MainGameFrame.java
+++ MainGameFrame.java
@@ -14,6 +14,7 @@ import javax.swing.SwingUtilities;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
@@ -27,6 +28,7 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainGameFrame extends JFrame{
 
@@ -34,15 +36,17 @@ public class MainGameFrame extends JFrame{
   JPanel mapPanel, auxPanel;
   JScrollPane narrationPanel;
   JTextArea narrationTextArea;
-  JPanel HUDPanel;
-  JPanel HUDTopPanel;
-  JPanel HUDArmySelectPanel, HUDCitySelectPanel, HUDResSelectPanel, HUDNoneSelectPanel;
+  JPanel HUDPanel, HUDTopPanel;
+  JPanel HUDLockedPanel, HUDArmySelectPanel, HUDCitySelectPanel, HUDResSelectPanel, HUDNoneSelectPanel;
   CardLayout HUDLayout;
+  JPanel HUDNoneSelectMiddlePanel;
+  JLabel currentGold, currentIncome;
   
   JPanel currentPositionPanel;
   JLabel currentPositionLabel;
   JPanel [][] gridSquares;
   Tile[][] mapTiles;
+  Tile currentSelected;
   
   GridSquareMouseListener gridSquareMouseListener;
   Integer numGridSquares;
@@ -51,6 +55,11 @@ public class MainGameFrame extends JFrame{
   final Integer AUX_SIZE = 190;
   final Integer NARRATION_FONT_SIZE = 11;
   final Integer POSITION_FONT_SIZE = 9;
+  
+  GetNameDialog getNameDialog;
+  String name;
+  static Integer playerIndex = 0;
+  static ArrayList<String> currentNames = new ArrayList<String>();
   
   public MainGameFrame(String inTitle, Integer numRows, Integer numCols, Tile[][] tiles){
     super(inTitle);
@@ -71,6 +80,14 @@ public class MainGameFrame extends JFrame{
     mapTiles = tiles;
 
     gridSquareMouseListener = new GridSquareMouseListener();
+    
+    name = "Player " + playerIndex.toString();
+    playerIndex++;
+    getNameDialog = new GetNameDialog(this);
+    if (currentNames.indexOf(getNameDialog.getInput()) == -1){
+      name = getNameDialog.getInput();
+      currentNames.add(name);
+    }
     
     gridSquares = new JPanel[ROWS][COLS];
     
@@ -125,8 +142,20 @@ public class MainGameFrame extends JFrame{
     currentPositionPanel = new JPanel();
     currentPositionPanel.add(currentPositionLabel);
     
+    HUDLockedPanel = new JPanel(new BorderLayout());
+    HUDLockedPanel.add(new JLabel("Waiting for other players", JLabel.CENTER), BorderLayout.CENTER);
+    
     HUDNoneSelectPanel = new JPanel(new BorderLayout());
-    HUDNoneSelectPanel.add(new JLabel("Default Information:", JLabel.CENTER), BorderLayout.NORTH);
+    HUDNoneSelectPanel.add(new JLabel(name + "'s turn", JLabel.CENTER), BorderLayout.NORTH);
+    currentGold = new JLabel("Current Gold: 0", JLabel.CENTER);
+    currentGold.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
+    currentIncome = new JLabel("Current Income: 0", JLabel.CENTER);
+    currentIncome.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
+
+    HUDNoneSelectMiddlePanel = new JPanel(new FlowLayout());
+    HUDNoneSelectMiddlePanel.add(currentGold);
+    HUDNoneSelectMiddlePanel.add(currentIncome);
+    HUDNoneSelectPanel.add(HUDNoneSelectMiddlePanel, BorderLayout.CENTER);
     
     HUDArmySelectPanel = new JPanel(new BorderLayout());
     HUDArmySelectPanel.add(new JLabel("Army Information:", JLabel.CENTER), BorderLayout.NORTH);
@@ -137,11 +166,12 @@ public class MainGameFrame extends JFrame{
     HUDResSelectPanel = new JPanel(new BorderLayout());
     HUDResSelectPanel.add(new JLabel("Resource Information:", JLabel.CENTER), BorderLayout.NORTH);
     
+    HUDTopPanel.add(HUDLockedPanel, "Lock");
     HUDTopPanel.add(HUDNoneSelectPanel, "None");
     HUDTopPanel.add(HUDArmySelectPanel, "Army");
     HUDTopPanel.add(HUDCitySelectPanel, "City");
     HUDTopPanel.add(HUDResSelectPanel, "Resource");
-    HUDLayout.show(HUDTopPanel, "None");
+    HUDLayout.show(HUDTopPanel, "Lock");
     
     HUDPanel.add(HUDTopPanel, BorderLayout.CENTER);
     HUDPanel.add(currentPositionPanel, BorderLayout.SOUTH);
@@ -158,24 +188,49 @@ public class MainGameFrame extends JFrame{
   
   public class GridSquareMouseListener extends MouseAdapter{
     public void mouseClicked(MouseEvent e) {
-      String rowNumber = null;
-      String colNumber = null;
+      Integer rowNumber = null;
+      Integer colNumber = null;
       for (Integer i = 0; i < ROWS; i++)
         for (Integer j = 0; j < COLS; j++)
           if (e.getSource() == gridSquares[i][j]){
-            rowNumber = i.toString();
-            colNumber = j.toString();
-            if (mapTiles[i][j].getOccupant() != null)//occupied by army
-              HUDLayout.show(HUDTopPanel, "army");
-            if (mapTiles[i][j].isCity())//is a city
-              HUDLayout.show(HUDTopPanel, "city");
-            if (mapTiles[i][j].isResource())//is a resource
-              HUDLayout.show(HUDTopPanel, "resource");
+            rowNumber = i;
+            colNumber = j;
+            if (currentSelected == null || currentSelected.getOccupant() == null){
+              //last map click wasn't an army
+              currentSelected = mapTiles[i][j];
+              if (currentSelected.getOccupant() != null){//occupied by army
+                HUDLayout.show(HUDTopPanel, "Army");
+                //box outline around
+              }
+              else if (currentSelected.isCity())//is a city
+                HUDLayout.show(HUDTopPanel, "City");
+              else if (currentSelected.isResource())//is a resource
+                HUDLayout.show(HUDTopPanel, "Resource");
+              else
+                HUDLayout.show(HUDTopPanel, "None");
+            }
+            else{
+              //last map click was an army
+              Army prevSelectedArmy = currentSelected.getOccupant();
+              currentSelected = mapTiles[i][j];
+              if (currentSelected.getOccupant() != null)//occupied by army
+                System.out.println("Army attacking army");
+              else if (currentSelected.isCity())//is a city
+                System.out.println("Army claims city");
+              else if (currentSelected.isResource())//is a resource
+                System.out.println("Army claims resource");
+              else
+                System.out.println("Army moving");
+            }
           }
-      if(SwingUtilities.isLeftMouseButton(e))
+      if(SwingUtilities.isLeftMouseButton(e)){
         System.out.println("left clicked on square at: (Row: " + rowNumber + " Col: " + colNumber + ")");
-      if(SwingUtilities.isRightMouseButton(e))
+        outlineAroundSquare(rowNumber, colNumber, 7);
+      }
+      if(SwingUtilities.isRightMouseButton(e)){
         System.out.println("right clicked on square at: (Row: " + rowNumber + " Col: " + colNumber + ")");
+        undoOutlineAroundSquare(rowNumber, colNumber, 7);
+      }
     }
   
     public void mouseEntered(MouseEvent e) {
@@ -208,7 +263,7 @@ public class MainGameFrame extends JFrame{
     gridSquares[pos.row][pos.col].setBackground(update.getColor());
     
     if (update.getOccupant() != null || true){
-      String occupantColor = "green";//update.getOccpant().getColor().toString();
+      String occupantColor = "green";//update.getOccpant().getColor();
       Integer strength = 70;//update.getOccupant().getStrength();
       
       BufferedImage armyImage = null;
@@ -244,10 +299,68 @@ public class MainGameFrame extends JFrame{
     BufferedImage dimg = new BufferedImage(size, size, img.getType());  
     Graphics2D g = dimg.createGraphics();  
     g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-    RenderingHints.VALUE_INTERPOLATION_BILINEAR);  
+        RenderingHints.VALUE_INTERPOLATION_BILINEAR);  
     g.drawImage(img, 0, 0, size, size, 0, 0, w, h, null);
     g.dispose();  
     return dimg;
+  }
+  
+  Color stringToColor(String in){
+    if (in.equals("plains"))
+      return Color.GREEN.darker();
+    else if (in.equals("forest"))
+      return Color.green.darker().darker();
+    else if (in.equals("muddy"))
+      return new Color(90, 90, 0);
+    else if (in.equals("mountain"))
+      return new Color(90, 50, 0);
+    else if (in.equals("water"))
+      return Color.BLUE;
+    else
+      return null;
+  }
+  
+  String getPlayerName(){
+    return name;
+  }
+  
+  void printGold(Integer gold){
+    currentGold.setText("Current Gold: " + gold);
+  }
+  void printIncome(Integer income){
+    currentIncome.setText("Current Income: " + income);
+  }
+  
+  void outlineAroundSquare(Integer row, Integer col, Integer radius){
+    gridSquares[row][col].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    for (int j = 0; j <= radius; j++){
+      for (int i = 0; i <= radius - j; i++){
+        if (row + j < ROWS && col + i < COLS)
+          gridSquares[row + j][col + i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        if (row + j < ROWS && col - i >= 0)
+          gridSquares[row + j][col - i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        if (row - j >= 0 && col + i < COLS)
+          gridSquares[row - j][col + i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        if (row - j >= 0 && col - i >= 0)
+          gridSquares[row - j][col - i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+      }
+    }
+  }
+  
+  void undoOutlineAroundSquare(Integer row, Integer col, Integer radius){
+    gridSquares[row][col].setBorder(null);
+    for (int j = 0; j <= radius; j++){
+      for (int i = 0; i <= radius - j; i++){
+        if (row + j < ROWS && col + i < COLS)
+          gridSquares[row + j][col + i].setBorder(null);
+        if (row + j < ROWS && col - i >= 0)
+          gridSquares[row + j][col - i].setBorder(null);
+        if (row - j >= 0 && col + i < COLS)
+          gridSquares[row - j][col + i].setBorder(null);
+        if (row - j >= 0 && col - i >= 0)
+          gridSquares[row - j][col - i].setBorder(null);
+      }
+    }
   }
 }
 
