ed3ab5b5c0f7618c58ea7944732f1b7199d78c3a
Jordan Liu <jminjie@umich.edu>
2013-12-02 14:33:54 -0500

    Actually commented GUI classes

--- GetNameDialog.java
+++ GetNameDialog.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -15,64 +14,66 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+//gets the name from the user and displays custom title card
 public class GetNameDialog extends JDialog {
 
-    JTextField inputField;
-    JButton okButton;
-    private static final long serialVersionUID = 1L;
+  JTextField inputField;
+  JButton okButton;
+  private static final long serialVersionUID = 1L;
 
-    public GetNameDialog(JFrame mainFrame) {
-        super(mainFrame, "", true);
+  public GetNameDialog(JFrame mainFrame) {
+    super(mainFrame, "", true);
 
-        ImageIcon title = new ImageIcon("src/Resources/titlecard.png");
+    ImageIcon title = new ImageIcon("src/Resources/titlecard.png");
 
-        JPanel topUpperPan = new JPanel(new FlowLayout());
-        JPanel topLowerPan = new JPanel(new FlowLayout());
-        JPanel topPan = new JPanel(new BorderLayout());
-        JPanel bottomPan = new JPanel(new FlowLayout());
-        setLayout(new BorderLayout());
-        okButton = new JButton("OK");
-        bottomPan.add(okButton);
-        topUpperPan.add(new JLabel("Player name:", SwingConstants.LEFT));
-        inputField = new JTextField(40);
-        topLowerPan.add(inputField);
+    JPanel topUpperPan = new JPanel(new FlowLayout());
+    JPanel topLowerPan = new JPanel(new FlowLayout());
+    JPanel topPan = new JPanel(new BorderLayout());
+    JPanel bottomPan = new JPanel(new FlowLayout());
+    setLayout(new BorderLayout());
+    okButton = new JButton("OK");
+    bottomPan.add(okButton);
+    topUpperPan.add(new JLabel("Player name:", SwingConstants.LEFT));
+    inputField = new JTextField(40);
+    topLowerPan.add(inputField);
+    //fill topPan
+    topPan.add(new JLabel(title), BorderLayout.NORTH);
+    topPan.add(topUpperPan, BorderLayout.CENTER);
+    topPan.add(topLowerPan, BorderLayout.SOUTH);
+    add(topPan, BorderLayout.CENTER);
+    add(bottomPan, BorderLayout.SOUTH);
 
-        topPan.add(new JLabel(title), BorderLayout.NORTH);
-        topPan.add(topUpperPan, BorderLayout.CENTER);
-        topPan.add(topLowerPan, BorderLayout.SOUTH);
-        add(topPan, BorderLayout.CENTER);
-        add(bottomPan, BorderLayout.SOUTH);
+    buttonListener listener = new buttonListener();
+    okButton.addActionListener(listener);
 
-        buttonListener listener = new buttonListener();
-        okButton.addActionListener(listener);
+    pack();
+    setResizable(false);
+    setLocation(400, 200);
+    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    getRootPane().setDefaultButton(okButton);
+    setVisible(true);
+  }
 
-        pack();
-        setResizable(false);
-        setLocation(400, 200);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        getRootPane().setDefaultButton(okButton);
-        setVisible(true);
-    }
-
-    public class buttonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == okButton) {
-                // only close window if user has entered a name
-                if (getInput().compareTo("") != 0) {
-                    // Send name to server
-                    try {
-                        Siege.sendToServer("NAME: " + getInput());
-                        System.out.println("Sending player name (" + getInput() + ") to server..");
-                    } catch (Exception e1) {
-                        System.out.println("ERROR: SENDING NAME TO SERVER");
-                    }
-                    setVisible(false);
-                }
-            }
+  public class buttonListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      if (e.getSource() == okButton) {
+        // only close window if user has entered a name
+        if (getInput().compareTo("") != 0) {
+          // Send name to server
+          try {
+            Siege.sendToServer("NAME: " + getInput());
+            System.out.println("Sending player name (" + getInput()
+                + ") to server..");
+          } catch (Exception e1) {
+            System.out.println("ERROR: SENDING NAME TO SERVER");
+          }
+          setVisible(false);
         }
+      }
     }
+  }
 
-    public String getInput() {
-        return inputField.getText();
-    }
+  public String getInput() {
+    return inputField.getText();
+  }
 }
